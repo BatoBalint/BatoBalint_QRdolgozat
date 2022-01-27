a66da2e205f376ee56ad2d9bc81faf21b15b94eb
BatoBalint <balint.bato@gmail.com>
2022-01-27 09:59:41 +0100

    gomb functionok inicializalva

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button scanBtn;
@@ -23,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
         scanBtn = findViewById(R.id.scanBtn);
         kiirBtn = findViewById(R.id.kiirBtn);
         txtView = findViewById(R.id.txtView);
+
+        scanBtn.setOnClickListener(v -> scanBtnClick());
+        kiirBtn.setOnClickListener(v -> kiirBtnClick());
+    }
+
+    private void scanBtnClick() {
+
+    }
+
+    private void kiirBtnClick() {
+
     }
 }
