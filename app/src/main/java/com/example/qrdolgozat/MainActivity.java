74f71d858da4bd0187bb379be7382422276a2b67
BatoBalint <balint.bato@gmail.com>
2022-01-27 21:47:48 +0100

    megy a qr scan

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.qrdolgozat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class MainActivity extends AppCompatActivity {
     private Button scanBtn;
     private Button kiirBtn;
     private TextView txtView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +37,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void scanBtnClick() {
-
+        IntentIntegrator qrIntent = new IntentIntegrator(this);
+        qrIntent.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        qrIntent.setPrompt("QR Scan");
+        qrIntent.setCameraId(0);
+        qrIntent.setBeepEnabled(false);
+        qrIntent.setOrientationLocked(false);
+        qrIntent.initiateScan();
     }
 
     private void kiirBtnClick() {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Scanner closed", Toast.LENGTH_SHORT).show();
+            } else {
+                txtView.setText(result.getContents());
+            }
+        }
     }
 }
