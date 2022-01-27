6eeb11440a0cdbd08265faea2543a77f3d21920d
BatoBalint <balint.bato@gmail.com>
2022-01-27 09:54:24 +0100

    gombok inicializalva

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.qrdolgozat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button scanBtn;
+    private Button kiirBtn;
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        scanBtn = findViewById(R.id.scanBtn);
+        kiirBtn = findViewById(R.id.kiirBtn);
+        txtView = findViewById(R.id.txtView);
     }
 }
